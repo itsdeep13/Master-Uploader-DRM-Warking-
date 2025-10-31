5b84b927510389bc658ef082aa9eefeccd8fb1c6
itsdeep13 <80030947+itsdeep13@users.noreply.github.com>
2025-10-31 10:37:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6047785902:AAE59KTfmhRvF8sUSYIzl9wcGnm4FLXiWDk")  # Ensure correct key name
-    API_ID = int(os.environ.get("API_ID", "24250238"))  # Added key name and default value
-    API_HASH = os.environ.get("API_HASH", "cb3f118ce5553dc140127647edcf3720")  # Added key name for consistency
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8460462838:AAFROgLrDyEIywR4KqNR9bW0QzI1O9cahcM")  # Ensure correct key name
+    API_ID = int(os.environ.get("API_ID", "22484497"))  # Added key name and default value
+    API_HASH = os.environ.get("API_HASH", "c38cb053916c47a97590c244663cbaef")  # Added key name for consistency
 
-    AUTH_USER = os.environ.get("AUTH_USERS", "6175650047").split(',')
+    AUTH_USER = os.environ.get("AUTH_USERS", "6252997817").split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]  # Ensuring list of integers
     
     HOST = os.environ.get("HOST", "https://lnkz.tech/")  # Keeping HOST configurable
     CREDIT = os.environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")  # Making CREDIT an environment variable for flexibility
+
 
 
